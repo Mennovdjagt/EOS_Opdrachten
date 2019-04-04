3fe387b34be53986b538b62757ca796030b19c07
Mennovdjagt <43231296+Mennovdjagt@users.noreply.github.com>
2019-04-04 19:16:39 +0200

    shell.hh aangemaakt

---
+++ Opdracht 1 OS Level Programming/shell.hh
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <unistd.h>  // syscall()
+#include <syscall.h> // SysCall nummers
+#include <fcntl.h>   // O_RDONLY
+#include <stdio.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+
+int main();
+void new_file();
+void list();
+void find();
+void python();
+void src();
